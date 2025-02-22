3f69796d729ef6669e048a97d8900e3c055e0bc5
SohamMhatre09 <ichbinsoham@gmail.com>
2025-02-22 17:39:22 +0530

    Soham Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Auth  from './Auth'
-import DriveFolderSelector from './components/DriveFolderSelector';
+import DriveFolderDownloader from './components/DriveFolderDownloader'
 
 function App() {
   return (
     <>
       {/* <Auth /> */}
       <div className="p-4">
-      <DriveFolderSelector />
+      {/* <DriveFolderDownloader /> */}
     </div>
     </>
   )
