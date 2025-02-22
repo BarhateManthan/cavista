ea314f47cbf7d9e06148c4fed637798da33dbc54
Mihir <mihir.pande22@pccoepune.org>
2025-02-22 14:32:32 +0530

    done with basic auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Auth  from './Auth'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Auth />
     </>
   )
 }
