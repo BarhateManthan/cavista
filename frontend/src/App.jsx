d01b40ddd4a6005eb3f9f110a4519bd03ea49145
Mihir <mihir.pande22@pccoepune.org>
2025-02-22 17:38:29 +0530

    added home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Auth  from './Auth'
-import DriveFolderSelector from './components/DriveFolderSelector';
+import Auth from './Auth'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <>
-      {/* <Auth /> */}
-      <div className="p-4">
-      <DriveFolderSelector />
-    </div>
+      <Home />
     </>
   )
 }
